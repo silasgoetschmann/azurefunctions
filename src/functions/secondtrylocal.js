c00d339455f6174b309257d2b1a5a2edd7fc1739
Goetschmann Silas <silas.goetschmann@ict-campus.net>
2025-06-06 15:01:52 +0200

    Update Database to personal db

--- src/functions/secondtrylocal.js
+++ src/functions/secondtrylocal.js
@@ -1,8 +1,8 @@
 const { app,input } = require('@azure/functions');
 
 const cosmosInput = input.cosmosDB({
-    databaseName: 'DemoDatabase',
-    containerName: 'Items',
+    databaseName: 'TestDB',
+    containerName: 'Table1',
     connection: 'CosmosDB',
     sqlQuery: "select * from c"
 });
